e9d815f11730f9d46dbbe53c83e4a316286a67ff
Bulaxy <rogerhiufungcheng@gmail.com>
2022-03-01 17:43:18 +1100

    Convert React Grid from Hook to Contexxt (Part 2)

--- src/components/Widgets/index.js
+++ src/components/Widgets/index.js
@@ -1,13 +1,29 @@
 import TableWidget from "./TableWidget"
+import Card from 'react-bootstrap/Card'
+import CloseButton from 'react-bootstrap/CloseButton'
+import { useReactGridContext } from "../../contexts/ReactGridContext"
 
 export default function WidgetComponents(props) {
-    const { widgetName } = props
+    const { widgetName, setting } = props
+    const { editMode } = useReactGridContext()
 
     const components = {
         TableWidget: TableWidget
     }
 
     const Widget = components[widgetName]
+
+    if (setting?.header || editMode) {
+        return <>
+            <Card.Header className='d-flex justify-content-between align-items-center pb-0'>
+                <Card.Title>{setting.header}</Card.Title>
+                {editMode && <CloseButton />}
+            </Card.Header>
+            <Card.Body className='p-0'>
+                <Widget />
+            </Card.Body>
+        </>
+    }
 
     return <Widget />
 }
---
+++ src/contexts/ReactGridContext.js
@@ -0,0 +1,33 @@
+import React, { useContext, useState } from "react"
+import useLocalStorage from "../hooks/useLocalStorage"
+
+const ReactGridContext = React.createContext()
+
+export const useReactGridContext = () => {
+    return useContext(ReactGridContext)
+}
+
+export const ReactGridProvider = ({ children }) => {
+    const [items, setItems] = useLocalStorage('RGLItems', [])
+    const [mode, setMode] = useState('view')
+
+    const editMode = mode === 'edit'
+    const toggleEditMode = () => {
+        setMode(editMode ? 'view' : 'edit')
+    }
+
+    return (
+        <ReactGridContext.Provider
+            value={{
+                items,
+                setItems,
+                mode,
+                setMode,
+                editMode,
+                toggleEditMode
+            }}
+        >
+            {children}
+        </ReactGridContext.Provider>
+    )
+}
